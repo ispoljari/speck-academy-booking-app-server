78effe0496c1da421533710079cd6e139de0cb0a
Matija Jaic <jaicmatija@gmail.com>
2019-06-02 15:31:37 +0200

    Add exporting of controlers

---
+++ route-handlers/index.js
@@ -0,0 +1,9 @@
+const hallsHandler = require("./halls/hallsController");
+const reservationsHandler = require("./reservations/reservationsController");
+const adminsHandler = require("./admins/adminsController");
+
+module.exports = {
+  hallsHandler,
+  reservationsHandler,
+  adminsHandler
+};
