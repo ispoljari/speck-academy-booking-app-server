c3632af1579e04050fe4bf05d44e4d1708128bf7
Matija Jaic <jaicmatija@gmail.com>
2019-06-02 17:48:38 +0200

    Remove enum from config

--- config.js
+++ config.js
@@ -2,16 +2,6 @@ const PORT = process.env.PORT || 8080;
 
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
 
-const HTTP_STATUS_CODES = {
-  OK: 200,
-  CREATED: 201,
-  NO_CONTENT: 204,
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-  NOT_FOUND: 404,
-  INTERNAL_SERVER_ERROR: 500
-};
-
 const DB_CONNECTION = {
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
@@ -22,7 +12,6 @@ const DB_CONNECTION = {
 
 module.exports = {
   PORT,
-  HTTP_STATUS_CODES,
   CLIENT_ORIGIN,
   DB_CONNECTION
 };
