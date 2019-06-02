1db21e200fb5bbc0fba6f578382e6448fa4c0591
Matija Jaic <jaicmatija@gmail.com>
2019-06-02 15:52:54 +0200

    Add admin controller

---
+++ route-handlers/admins/adminsController.js
@@ -0,0 +1,29 @@
+const db = require("../../db/connect");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const { HTTP_STATUS_CODES } = require("../../config");
+
+const adminLogin = async (request, response) => {
+  const { userName, password } = request.body;
+
+  const dbResponse = await db.query(
+    "SELECT * FROM Admins WHERE user_name = $1",
+    [userName]
+  );
+  if (dbResponse.rows.length === 0) {
+    throw new Error("Invalid credentials");
+  }
+  const admin = dbResponse.rows[0];
+  const { hashed_password: hashedPassword } = admin;
+
+  if (!(await bcrypt.compare(password, hashedPassword))) {
+    throw new Error("Invalid credentials");
+  }
+  // TODO: send token in response.Check if token has expired.
+};
+
+const router = new express.Router();
+
+router.route("/login").post(adminLogin);
+
+module.exports = router;
