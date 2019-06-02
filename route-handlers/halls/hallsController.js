e1a93598fb73b4111e44f1e9fd9825561a5240b2
Matija Jaic <jaicmatija@gmail.com>
2019-06-02 18:03:36 +0200

    Replace promises with async await

--- route-handlers/halls/hallsController.js
+++ route-handlers/halls/hallsController.js
@@ -1,75 +1,81 @@
 const db = require("../../db/connect");
 const express = require("express");
-const { HTTP_STATUS_CODES } = require("../../config");
+const HTTP_STATUS_CODES = require("../../enums/http_status_codes");
+const _ = require("lodash");
 
-const getHalls = (request, response) => {
-  db.query("SELECT * FROM Halls ORDER BY id ASC")
-    .then(res => response.status(HTTP_STATUS_CODES.OK).json(res.rows))
-    .catch(error => {
-      throw error;
-    });
+const getHalls = async (request, response) => {
+  const dbResponse = await db.query("SELECT * FROM Halls ORDER BY id ASC");
+  response.status(HTTP_STATUS_CODES.OK).json(dbResponse.rows);
 };
 
-const getHallById = (request, response) => {
+const getHallById = async (request, response) => {
   const id = parseInt(request.params.id);
-  db.query("SELECT * FROM Halls WHERE id = $1", [id])
-    .then(res => response.status(HTTP_STATUS_CODES.OK).json(res.rows[0]))
-    .catch(error => {
-      throw error;
-    });
+  const dbResponse = await db.query("SELECT * FROM Halls WHERE id = $1", [id]);
+  response.status(HTTP_STATUS_CODES.OK).json(dbResponse.rows[0]);
 };
 
-const createHall = (request, response) => {
+const createHall = async (request, response) => {
   const { name, address, pictureUrl, description } = request.body;
-
-  db.query(
-    "INSERT INTO Halls (name, address, picture_url, description) VALUES ($1, $2, $3, $4)",
+  await db.query(
+    `INSERT INTO Halls (name, address, picture_url, description) 
+  VALUES ($1, $2, $3, $4)`,
     [name, address, pictureUrl, description]
-  )
-    .then(res => response.status(HTTP_STATUS_CODES.CREATED).json({}))
-    .catch(error => {
-      throw error;
-    });
+  );
+  response.status(HTTP_STATUS_CODES.CREATED).json({});
 };
 
-const updateHall = (request, response) => {
+const updateHall = async (request, response) => {
   const id = parseInt(request.params.id);
+  const dbResponse = await db.query("SELECT * FROM Halls WHERE id = $1", [id]);
+  const hall = dbResponse.rows[0];
+  if (!hall) {
+    throw new Error("Hall does not exist");
+  }
+
   const { name, address, pictureUrl, description } = request.body;
+  // TODO: upload picture
+  Object.assign(
+    hall,
+    _.omitBy(
+      {
+        name,
+        address,
+        picture_url: pictureUrl,
+        description
+      },
+      _.isUndefined
+    )
+  );
+
   const updatedAt = new Date();
 
-  db.query(
+  await db.query(
     "UPDATE Halls SET name = $1, address = $2, picture_url = $3, description = $4, updated_at = $5 WHERE id = $6",
-    [name, address, pictureUrl, description, updatedAt, id]
-  )
-    .then(res => response.status(HTTP_STATUS_CODES.OK).json({}))
-    .catch(error => {
-      throw error;
-    });
+    [hall.name, hall.address, hall.picture_url, hall.description, updatedAt, id]
+  );
+  response.status(HTTP_STATUS_CODES.OK).json({});
 };
 
-const deleteHall = (request, response) => {
+const deleteHall = async (request, response) => {
   const id = parseInt(request.params.id);
-
-  db.query("DELETE FROM Halls WHERE id = $1", [id])
-    .then(() => response.status(HTTP_STATUS_CODES.OK).json({}))
-    .catch(error => {
-      throw error;
-    });
+  await db.query("DELETE FROM Halls WHERE id = $1", [id]);
+  response.status(HTTP_STATUS_CODES.OK).json({});
 };
 
-const getHallsWithReservationsByReservationDateRange = (request, response) => {
+const getHallsWithReservationsByReservationDateRange = async (
+  request,
+  response
+) => {
   const { startDate, endDate } = request.body;
-  db.query(
-    `SELECT Halls.*, array_agg(reservations.*) as hall_reservations
-        FROM Halls JOIN reservations ON Halls.id = hall_fk
-        WHERE date BETWEEN $1 AND $2
-        GROUP BY Halls.id`,
+  const dbResponse = await db.query(
+    `
+    SELECT Halls.*, array_agg(reservations.*) as hall_reservations
+    FROM Halls JOIN reservations ON Halls.id = hall_fk
+    WHERE date BETWEEN $1 AND $2
+    GROUP BY Halls.id`,
     [startDate, endDate]
-  )
-    .then(res => response.status(HTTP_STATUS_CODES.OK).json(res.rows))
-    .catch(error => {
-      throw error;
-    });
+  );
+  response.status(HTTP_STATUS_CODES.OK).json(dbResponse.rows);
 };
 
 const router = new express.Router();
