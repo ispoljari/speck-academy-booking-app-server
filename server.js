64f7c05d49b8dde9fc782c51315cd9b534ae5303
Matija Jaic <jaicmatija@gmail.com>
2019-06-02 15:11:44 +0200

    Init server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-// test code
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 const cors = require("cors");
-
 const app = express();
 const { PORT, CLIENT_ORIGIN, HTTP_STATUS_CODES } = require("./config");
+const {
+  hallsHandler,
+  reservationsHandler,
+  adminsHandler
+} = require("./route-handlers");
+
+const router = new express.Router();
 
 app.use(bodyParser.json());
 app.use(
@@ -21,16 +24,19 @@ app.use(
   })
 );
 
-// demo endpoints
-
-app.get("/api/users", (req, res) => {
-  return res.json("Here is your list of users");
+// Base routes
+app.use((req, res, next) => {
+  // TODO: find admins by token.
+  next();
 });
+app.use("/api/halls", hallsHandler);
+app.use("/api/reservations", reservationsHandler);
+app.use("/api/admins", adminsHandler);
 
-app.get("/api/halls", (req, res) => {
-  return res.json("Here is your list of halls");
-});
+app.use("*", (req, res) =>
+  res.status(HTTP_STATUS_CODES.NOT_FOUND).json({
+    message: "Invalid URL"
+  })
+);
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server Listening on port ${PORT}!`));
