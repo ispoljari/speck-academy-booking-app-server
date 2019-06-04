7141c12bfbd71f3ad9421283e71a40e1a89d59c8
Matija Jaic <jaicmatija@gmail.com>
2019-06-04 03:51:32 +0200

    add index.js and refactor code for halls route handlers

---
+++ route-handlers/halls/controller.js
@@ -0,0 +1,127 @@
+const hallRepository = require("../../repositories/halls");
+const { HTTP_STATUS_CODES } = require("../../enums");
+const _ = require("lodash");
+
+const getHalls = async (request, response) => {
+  const halls = await hallRepository.getAll();
+  response.status(HTTP_STATUS_CODES.OK).json(halls);
+};
+
+const getHallById = async (request, response, next) => {
+  const id = parseInt(request.params.id);
+  if (isNaN(id)) {
+    next({
+      status: HTTP_STATUS_CODES.BAD_REQUEST,
+      message: "id should be a number"
+    });
+    return;
+  }
+  const hall = await hallRepository.getById(id);
+  if (!hall) {
+    next({
+      status: HTTP_STATUS_CODES.BAD_REQUEST,
+      message: "Hall with that id does not exist"
+    });
+    return;
+  }
+  response.status(HTTP_STATUS_CODES.OK).json(hall);
+};
+
+const createHall = async (request, response) => {
+  const { name, address, pictureUrl, description } = request.body;
+  await hallRepository.create(name, address, pictureUrl, description);
+  response.status(HTTP_STATUS_CODES.CREATED).json({});
+};
+
+const updateHall = async (request, response, next) => {
+  const id = parseInt(request.params.id);
+  const hall = await hallRepository.getById(id);
+  if (!hall) {
+    next({
+      status: HTTP_STATUS_CODES.BAD_REQUEST,
+      message: "Hall with that id does not exist"
+    });
+    return;
+  }
+
+  const { name, address, pictureUrl, description } = request.body;
+  // TODO: upload picture
+  Object.assign(
+    hall,
+    _.omitBy(
+      {
+        name,
+        address,
+        pictureUrl,
+        description
+      },
+      _.isUndefined
+    )
+  );
+
+  await hallRepository.update(
+    hall.name,
+    hall.address,
+    hall.pictureUrl,
+    hall.description,
+    id
+  );
+  response.status(HTTP_STATUS_CODES.OK).json({});
+};
+
+const deleteHall = async (request, response, next) => {
+  const id = parseInt(request.params.id);
+  const hall = await hallRepository.getById(id);
+  if (!hall) {
+    next({
+      status: HTTP_STATUS_CODES.BAD_REQUEST,
+      message: "Hall with that id does not exist"
+    });
+    return;
+  }
+
+  await hallRepository.deleteById(id);
+  response.status(HTTP_STATUS_CODES.OK).json({});
+};
+
+const getHallsWithReservationsByReservationDateRange = async (
+  request,
+  response
+) => {
+  const { startDate, endDate } = request.body;
+  const hallsWithReservationsByReservationDateRange = await hallRepository.getAllWithReservationsByReservationDateRange(
+    startDate,
+    endDate
+  );
+  response
+    .status(HTTP_STATUS_CODES.OK)
+    .json(hallsWithReservationsByReservationDateRange);
+};
+
+const getHallByIdWithReservations = async (request, response, next) => {
+  const id = parseInt(request.params.id);
+  const hall = await hallRepository.getById(id);
+  if (!hall) {
+    next({
+      status: HTTP_STATUS_CODES.BAD_REQUEST,
+      message: "Hall with that id does not exist"
+    });
+    return;
+  }
+  const { reservationDate } = request.body;
+  const hallWithReservations = await hallRepository.getByIdWithReservations(
+    id,
+    reservationDate
+  );
+  response.status(HTTP_STATUS_CODES.OK).json(hallWithReservations);
+};
+
+module.exports = {
+  getHalls,
+  getHallById,
+  createHall,
+  updateHall,
+  deleteHall,
+  getHallsWithReservationsByReservationDateRange,
+  getHallByIdWithReservations
+};
