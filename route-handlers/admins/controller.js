89065a8ea33cedaf260c4b06d6f2089e22a6c81d
Matija Jaic <jaicmatija@gmail.com>
2019-06-05 20:50:04 +0200

    Add login functionality

--- route-handlers/admins/controller.js
+++ route-handlers/admins/controller.js
@@ -1,18 +1,37 @@
 const adminsRepository = require("../../repositories/admins");
+const uuidv4 = require("uuid/v4");
 const bcrypt = require("bcrypt");
+const { DateTime } = require("luxon");
+const sessionsRepository = require("../../repositories/sessions");
 const { HTTP_STATUS_CODES } = require("../../enums");
 
 const adminLogin = async (request, response) => {
-  const { userName, password } = request.body;
+  try {
+    const { userName, password } = request.body;
+    const admin = await adminsRepository.getByUserName(userName);
+    if (!admin) {
+      throw new Error("Invalid credentials");
+    }
+    if (!(await bcrypt.compare(password, hashedPassword))) {
+      throw new Error("Invalid credentials");
+    }
+    const uuid = uuidv4();
+    const loginTimestamp = DateTime.local;
+    const expiryDate = loginTimestamp.plus({ days: 1 });
 
-  const admin = await adminsRepository.getByUserName(userName);
-  if (!admin) {
-    throw new Error("Invalid credentials");
+    await sessionsRepository.create(
+      uuid,
+      admin.id,
+      loginTimestamp.toISO(),
+      expiryDate.toISO()
+    );
+    response.cookie("sessionId", uuid);
+    response.status(HTTP_STATUS_CODES.OK).json({});
+  } catch (error) {
+    response.status(HTTP_STATUS_CODES.BAD_REQUEST).json({
+      message: error.message
+    });
   }
-  if (!(await bcrypt.compare(password, hashedPassword))) {
-    throw new Error("Invalid credentials");
-  }
-  // TODO: send token in response.Check if token has expired.
 };
 
 module.exports = {
